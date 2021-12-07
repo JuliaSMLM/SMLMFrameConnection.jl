564fb0dc20b19fe77281cda2c2b02c3b250c79c4
Documenter.jl <documenter@juliadocs.github.io>
2021-12-07 21:33:52 +0000

    build based on 1a6ba56

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SMLMFrameConnection","category":"page"},{"location":"#SMLMFrameConnection","page":"Home","title":"SMLMFrameConnection","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SMLMFrameConnection.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SMLMFrameConnection]","category":"page"}]
+}
