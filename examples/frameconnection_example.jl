8d9ca3450961aee38c5a58393c79f62479d6817a
dschodt <35635770+dschodt@users.noreply.github.com>
2022-02-09 20:20:46 -0700

    Update example to remove testing code

--- examples/frameconnection_example.jl
+++ examples/frameconnection_example.jl
@@ -2,21 +2,21 @@
 
 using SMLMFrameConnection
 using SMLMSim
-using PlotlyJS
+using SMLMData
+using ImageView
 
 ## Simulate some data using SMLMSim.
 # Define fluorophore kinetics. 
 γ = 1e3 # emission rate, photons/frame
-q = [1.0-0.7 0.5 0.2
-    5.0e-3 1.0-5.0e-3 0.0
-    0.0 0.0 1.0] # kinetic transmissions (1/frame): [on->on, on->off, on->bleach; off->on, off->off, off->bleach; bleach->on, bleach->off, bleach->bleach]
+q = [0.0 0.5
+    5.0e-3 0.0] # kinetic transmissions (1/frame): [on->on, on->off; off->on, off->off]
 fluor = SMLMSim.GenericFluor(γ, q)
 
 # Define our pattern.
 pattern = SMLMSim.Point2D()
-ρ = 10.0 # emitters / pixel^2
-xsize = 32.0 # pixels
-ysize = 32.0
+ρ = 1.0 # emitters / pixel^2
+xsize = 8.0 # pixels
+ysize = 8.0
 smld_true = SMLMSim.uniform2D(ρ, pattern, xsize, ysize)
 smld_true.datasize = [ysize; xsize] # not populated by SMLMSim
 
@@ -24,36 +24,26 @@ smld_true.datasize = [ysize; xsize] # not populated by SMLMSim
 nframes = 2000
 framerate = 1.0 # I want units of frames, so setting to 1.0
 smld_model = SMLMSim.kineticmodel(smld_true, fluor, nframes, framerate; ndatasets = 1)
+smld_model.datasize = [ysize; xsize] # not populated by SMLMSim
 
 # Make noisy coordinates.
 σ_psf = 1.3 # st. dev. of Gaussian PSF, pixels
 smld_noisy = SMLMSim.noise(smld_model, σ_psf)
 smld_noisy.bg = zeros(Float64, length(smld_noisy.framenum)) # not populated in SMLMSim
+smld_noisy.σ_bg = fill(Inf64, length(smld_noisy.framenum))
+smld_noisy.σ_photons = fill(Inf64, length(smld_noisy.framenum))
+smld_noisy.datasize = [ysize; xsize] # not populated by SMLMSim
 
 # Perform frame connection.
 params = SMLMFrameConnection.ParamStruct()
-smld_combined, smld, smld_preclustered = SMLMFrameConnection.frameconnect(smld_noisy)
+smld_combined, smld_connected, smld_preclustered = SMLMFrameConnection.frameconnect(smld_noisy)
 
-# # Load some data and store it in an smd struct|ure.
-# filename = "C:\\Users\\David\\Documents\\work_stuff\\frame_connection\\actin_data.csv"
-# # filename = "C:\\Users\\David\\Documents\\work_stuff\\frame_connection\\uniform_rho10.csv"
-
-# data = DataFrames.DataFrame(CSV.File(filename))
-# smd = FrameConnection.SMD(data)
-
-# # Perform frame connection.
-# params = FrameConnection.ParamStruct()
-# smd_combined, smd, smd_preclustered = FrameConnection.frameconnect(smd)
-
-# # Make some images.
-# using SMLMData
-# using Images
-# using ImageView
-
-# circleim_in = SMLMData.makecircleim(Float64.([smd.y smd.x]), Float64.(vec(mean([smd.y_se smd.x_se], dims = 2))), [128; 128])
-# circleim_out = SMLMData.makecircleim(Float64.([smd_combined.y smd_combined.x]),
-#     Float64.(vec(mean([smd_combined.y_se smd_combined.x_se], dims = 2))), [128; 128])
-# circleim_in ./= maximum(circleim_in)
-# circleim_out ./= maximum(circleim_out)
-# testim = Images.RGB.(circleim_in, circleim_out, circleim_in)
-# ImageView.imshow(testim)
+## Make some plots of the results.
+# Plot the combined results overlain with the original data.
+mag = 50.0 # image magnification
+circleim_original = SMLMData.makecircleim(smld_noisy, mag)
+circleim_combined = SMLMData.makecircleim(smld_combined, mag)
+smld_gt = SMLMFrameConnection.combinelocalizations(smld_noisy)
+circleim_gt = SMLMData.makecircleim(smld_gt, mag)
+ImageView.imshow(ImageView.RGB.(circleim_original, circleim_combined, circleim_original))
+ImageView.imshow(ImageView.RGB.(circleim_gt, circleim_combined, circleim_gt))
