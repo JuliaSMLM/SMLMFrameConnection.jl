401587c0afb52badf5d1ae1b4d815e2f3c05730c
dschodt <35635770+dschodt@users.noreply.github.com>
2022-02-09 19:00:11 -0700

    Fix a bug in compress_connectID.jl

--- src/compress_connectID.jl
+++ src/compress_connectID.jl
@@ -9,7 +9,7 @@ function compress_connectID!(connectID::Vector{Int64})
     # Force `connectID` to consist of the integers 
     # `1:length(unique(connectID))` without skipping any integers.
     sortindices = sortperm(connectID)
-    connectID_sorted = connectID[sortindices]
+    connectID_sorted = deepcopy(connectID[sortindices])
     connectIDlist = unique(connectID_sorted)
     nperID = counts(connectID_sorted)
     nperID = nperID[nperID.!=0]
@@ -18,7 +18,12 @@ function compress_connectID!(connectID::Vector{Int64})
         clusterind = (1:nperID[nn]) .+ ncumulative[nn]
         connectID_sorted[clusterind] .= nn
     end
-    connectID[sortindices] = connectID_sorted
+    println(sortindices)
+    println(connectID_sorted)
+    println(connectID)
+    connectID[sortindices] = deepcopy(connectID_sorted)
+
+    return connectID
 end
 
 """
