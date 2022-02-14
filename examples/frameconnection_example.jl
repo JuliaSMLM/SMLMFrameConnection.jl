be6be7eca3e514ae875642785b5f3382d50ebedd
dschodt <35635770+dschodt@users.noreply.github.com>
2022-02-14 16:32:08 -0700

    Update example to use new SMLMSim interface

--- examples/frameconnection_example.jl
+++ examples/frameconnection_example.jl
@@ -7,34 +7,39 @@ using ImageView
 
 
 ## Simulate some data using SMLMSim.
-# Define fluorophore kinetics. 
+# Define simulation parameters.
+# NOTE: Care must be taken to maintain unit conventions - SMLMSim works in
+#       physical units, but here we're maintaining units of pixels and frames.
 γ = 1e3 # emission rate, photons/frame
 q = [0.0 0.5
     1.0e-3 0.0] # kinetic transmissions (1/frame): [on->on, on->off; off->on, off->off]
-fluor = SMLMSim.GenericFluor(γ, q)
-
-# Define our pattern.
+fluor = SMLMSim.GenericFluor(; γ = γ, q = q)
 pattern = SMLMSim.Point2D()
-ρ = 1.0 # emitters / pixel^2
-xsize = 8.0 # pixels
-ysize = 8.0
-smld_true = SMLMSim.uniform2D(ρ, pattern, xsize, ysize)
-smld_true.datasize = [ysize; xsize] # not populated by SMLMSim
-
-# Simulate fluorophore kinetics.
+ρ = 5.0 # emitters / pixel^2
+xsize = 8 # pixels
+ysize = 8
+pixelsize = 1.0 # 1.0 to keep units of pixels
+framerate = 1.0 # 1.0 to keep units of frames
 nframes = 10000
-ndatasets = 1 # should be 1 for now due to bug in defineidealFC()!
-framerate = 1.0 # I want units of frames, so setting to 1.0
-smld_model = SMLMSim.kineticmodel(smld_true, fluor, nframes, framerate; ndatasets = ndatasets)
-smld_model.datasize = [ysize; xsize] # not populated by SMLMSim
+ndatasets = 1 # for now, must be 1 due to unresolved bug in defineidealFC()
 
-# Make noisy coordinates from the kinetic model.
-σ_psf = 1.3 # st. dev. of Gaussian PSF, pixels
-smld_noisy = SMLMSim.noise(smld_model, σ_psf)
-smld_noisy.bg = zeros(Float64, length(smld_noisy.framenum)) # not populated in SMLMSim
+# Generate the simulation
+smld_true, smld_model, smld_noisy = SMLMSim.sim(;
+    ρ = ρ,
+    σ_PSF = 1.3,
+    minphotons = 50,
+    ndatasets = ndatasets,
+    nframes = nframes,
+    framerate = framerate,
+    pattern = pattern,
+    molecule = fluor,
+    camera = SMLMSim.IdealCamera(; xpixels = xsize, ypixels = ysize, pixelsize = pixelsize)
+)
+
+# Populate some missing fields not added during the simulation.
+smld_noisy.bg = zeros(Float64, length(smld_noisy.framenum))
 smld_noisy.σ_bg = fill(Inf64, length(smld_noisy.framenum))
 smld_noisy.σ_photons = fill(Inf64, length(smld_noisy.framenum))
-smld_noisy.datasize = [ysize; xsize] # not populated by SMLMSim
 
 # Perform frame connection.
 smld_connected, smld_preclustered, smld_combined, params = SMLMFrameConnection.frameconnect(smld_noisy;
